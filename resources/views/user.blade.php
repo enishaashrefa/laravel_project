77ec7d688c13ab938552def3a6b19fddf5a377df
enishaashrefa <61018044+enishaashrefa@users.noreply.github.com>
2021-10-05 21:57:18 +0600

    Initial commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html5>
+<html>
+
+<head>
+    <title>Enisha Ashrefa</title>
+    <style>
+        body {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        .grid-container {
+            display: grid;
+            grid-template-columns: auto auto;
+            grid-gap: 0px;
+            /* background-color: rgba(238, 232, 231, 0.911);
+          padding: 10px; */
+        }
+
+        .grid-container>div {
+
+            text-align: center;
+            padding: 20px 0;
+            font-size: 30px;
+
+        }
+
+        .col1 {
+            width: 450px;
+            height: 1540px;
+            background-color: rgb(30, 38, 43);
+            color: rgb(214, 95, 95);
+        }
+
+        .col2 {
+            width: 800px;
+            height: 1540px;
+            background-color: white;
+
+        }
+
+        .right {
+            text-align: left;
+            margin-top: 50px;
+            padding-left: 50px;
+            /*margin-right: -10px;
+            margin-left: 14%;*/
+            height: auto;
+            width: 99%;
+            color: rgb(214, 95, 95);
+        }
+
+        .right2 {
+            text-align: left;
+            margin-top: 5px;
+            padding-left: 50px;
+            /*margin-right: -10px;
+            margin-left: 14%;*/
+            height: auto;
+            width: 99%;
+            color: rgb(214, 95, 95);
+
+        }
+
+        .right3 {
+            text-align: left;
+            margin-top: 5px;
+
+            /*margin-right: -10px;
+            margin-left: 14%;*/
+            height: auto;
+            width: 99%;
+            color: rgb(214, 95, 95);
+        }
+
+        .right4 {
+            text-align: left;
+            margin-top: 5px;
+
+            /*margin-right: -10px;
+            margin-left: 14%;*/
+            height: auto;
+            width: 99%;
+            color: rgb(214, 95, 95);
+        }
+
+        .para {
+            font-size: small;
+            text-align: center;
+            margin-top: 10px;
+
+            /*margin-right: -10px;
+            margin-left: 14%;*/
+            height: auto;
+            width: 99%;
+        }
+
+        .para1 {
+            font-size: small;
+            color: black;
+        }
+
+        .para2 {
+            font-size: small;
+            color: black;
+        }
+
+        .para3 {
+            font-size: small;
+            color: black;
+        }
+
+        .para4 {
+            font-size: small;
+            color: black;
+        }
+
+        /*.para6{
+            font-size: small;
+            color: white;
+           
+            padding-left: 50px;
+            padding-right: 50px;
+        }
+        .para7{
+            font-size: small;
+            color: white;
+           
+            padding-left: 50px;
+            padding-right: 50px;
+        }
+        .skill{
+            text-align: left;
+            margin-top: 30px;
+            
+            margin-right: -10px;
+            margin-left: 10%;
+            height: auto;
+            width: 99%;
+            color:  rgb(214, 95, 95);
+        }
+        Lan{
+            text-align: left;
+            margin-top: 30px;
+            margin-right: -10px;
+            margin-left: 10%;
+            height: auto;
+            width: 99%;
+            color:  rgb(214, 95, 95);
+        }*/
+        .contact {
+            text-align: left;
+            margin-top: 30px;
+            margin-right: -10px;
+            margin-left: 10%;
+
+
+            height: auto;
+            width: 99%;
+            color: rgb(214, 95, 95);
+
+        }
+
+        .para8 {
+            font-size: small;
+            color: white;
+
+            padding-left: 50px;
+            padding-right: 50px;
+
+        }
+
+        a {
+            color: rgb(214, 95, 95);
+        }
+    </style>
+</head>
+
+<body>
+
+
+    <div class="grid-container">
+        <div class="col1">
+            <img src="Enisha Ashrefa.png" height="100px" width="100px">
+            <h5>Enisha Ashrefa</h5>
+            <div class="para">
+                <p>Computer Science Engineer</p>
+            </div>
+
+            <div class="contact">
+                <h6>Area of Interest</h6>
+                <div class="para8">
+                    <p>Machine Learning, Research, Programming, Web Development </p>
+                </div>
+
+
+            </div>
+            <div class="contact">
+                <h6>Skills</h6>
+                <div class="para8">
+                    <p>Python, Deep Learning, Scikit-learn, TensorFlow, Machine Learning, C/C++, Java, PHP,
+                        HTML, CSS, JavaScript, Codeigniter, MySQL, Laravel, MVC, MS office, Git
+                        & Github, Linux</p>
+
+                </div>
+
+
+            </div>
+
+            <div class="contact">
+                <h6>Achievements</h6>
+                <div class="para8">
+                    <p>Scholarship Award by United International University (Based on Trimester Result) </p>
+                </div>
+
+
+            </div>
+
+            <div class="contact">
+                <h6>Languages</h6>
+                <div class="para8">
+                    <p>Bangla(Native), English(Professional Fluency)</p>
+                </div>
+
+
+            </div>
+            <div class="contact">
+                <h6>Hobbies</h6>
+                <div class="para8">
+                    <p>Reading, Learning, Music.</p>
+                </div>
+
+
+            </div>
+
+            <div class="Contact">
+                <h6>Contact Information</h6>
+                <div class="para8">
+                    <ul>
+                        <li>
+                            <p>Mobile: 01858525544</p>
+                        </li>
+                        <li>
+                            <p> Email:
+                            <ul>
+                                <li>eashrefa171057@bscse.uiu.ac.bd</li>
+                                <li>enishaashrefa@gmail.com</li>
+                            </ul>
+                            </p>
+                        </li>
+                        <li>
+                            <p>Github Link: <a href="https://github.com/enishaashrefa"> www.github.com/enishaashrefa</a>
+                            </p>
+                        </li>
+                        <li>
+                            <p>Linkedin Link: <a
+                                    href="https://www.linkedin.com/in/enishaashrefa/">www.linkedin.com/in/enishaashrefa</a>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+
+
+            </div>
+
+
+            <div class="contact">
+                <h6>References</h6>
+                <div class="para8">
+                    <ul>
+                        <li>
+                            <p><b>Mirajul Islam </p></b>
+                            <p>Software Engineer at marginedge</p>
+                            <a
+                                href="https://sites.google.com/site/mirajul1995">https://sites.google.com/site/mirajul1995</a>
+                            <p>Email: mirajul1995@gmail.com</p><br>
+
+
+
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+
+
+
+        </div>
+        <div class="col2">
+            <div class="right">
+                <h6>Education</h6>
+                <div class="para1">
+                    <ul>
+                        <li>
+                            <p><b>United International University, Dhaka, Bangladesh </p></b>
+                            <p> BSc in Computer Science and Engineering</p>
+                            <p> CGPA: 3.73 in the scale of 4.00</p>
+                            <p> 24th January, 2017 - 29th July, 2021</p>
+                        </li>
+
+                        <li>
+                            <p><b>Brahmanbaria Govt. College, Brahmanbaria, Bangladesh</p></b>
+                            <p> HSC in Science</p>
+                            <p>GPA: 4.70 in the scale of 5.00</p>
+                        </li>
+
+                        <li>
+                            <p><b>Ideal Residential Scool And College Brahmanbaria, Brahmanbaria, Bangladesh</p></b>
+                            <p> SSC in Science</p>
+                            <p>GPA: 5.00 in the scale of 5.00</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right2">
+                <h6>Experiences</h6>
+                <div class="para2">
+                    <ul>
+                        <li>
+                            <p><b>UIU APP FORUM </p></b>
+                            <p> Treasurer (June 2021 – Present)</p>
+
+                        </li>
+                        <li>
+                            <p><b>UIU APP FORUM </p></b>
+                            <p> Instructor (November 2021 – Present)</p>
+
+                        </li>
+                        <li>
+                            <p><b>UIU APP FORUM </p></b>
+                            <p> Associate Public Relations (February 2020 – june 2021)</p>
+
+                        </li>
+                    </ul>
+                </div>
+                <div class="right3">
+                    <h6>Events I have participated in</h6>
+                    <div class="para3">
+                        <ul>
+                            <li>
+                                <p><b>Programming for beginners (P4B) - (Season 2 - Season 4) </p></b>
+                                <p>Coordinator, Judge</p>
+                                <p>This is a training event of UIUAPPF which has a project show at the end of the
+                                    training session (organized every trimester)</p>
+
+                            </li>
+                            <li>
+                                <p><b>Roadmap to higher Studies </p></b>
+                                <p>Coordinator, Scriptwriter</p>
+                                <p>It’s a live event organized by UIUAPPF which consists of one episode</p>
+
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="right5">
+                    <h6>Undergrad Projects</h6>
+                    <div class="para4">
+                        <ul>
+                            <li>
+                                <p><b>CCOJ online judge</p></b>
+                                <p>PHP, HTML, CSS, MySQL</p>
+                            </li>
+                            <li>
+                                <p><b>U-Queue </p></b>
+                                <p>CodeIgniter, HTML, CSS, JavaScript</p>
+
+
+                            </li>
+                            <li>
+                                <p><b>UIU CGPA-Run Game</p></b>
+                                <p>C Programming Language</p>
+
+
+                            </li>
+                            <li>
+                                <p><b>Electronics Lab Project: Automatic Garbage Management Robot</p></b>
+                                <p>Arduino, Sensors</p>
+
+
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="right2">
+                    <h6>Undergrad Thesis</h6>
+                    <div class="para2">
+                        <p><strong>Supervised By:</strong> Md. Rakibul Haque</p>
+                        <p><strong>Co-Supervised By:</strong> Dr. Arif Reza Anwary</p>
+                        <p><strong>Research Topic Based On:</strong> Machine Learning, Bio-Mechanics</p>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+            <!--<div class="right2">
+                <h6>Certification Link</h6>
+                <div class="para2">
+                    <p><a
+                            href="https://drive.google.com/drive/folders/1IQxLMzuPRDiKmUZZ4xCp5HFqPGlnGdOk?usp=sharing">https://drive.google.com/drive/folders/1IQxLMzuPRDiKmUZZ4xCp5HFqPGlnGdOk?usp=sharing</a>
+                    </p>
+                </div>
+            </div>
+            <div class="right2">
+                <h6>Ongoing Reasearch Work</h6>
+                <div class="para2">
+                    <p><strong>Based On:</strong> Machine Learning, Computer Vision</p>
+                </div>
+            </div>-->
+
+
+</body>
+
+</html>
